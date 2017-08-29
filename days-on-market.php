4503c8526b7e8f7afcc52bd0f51d522a5171c679
Aaron Huisinga <aaron@platform.marketing>
2017-08-29 13:04:10 -0500

    Bump to version 1.7.8
    1. Update Composer dependencies

--- days-on-market.php
+++ days-on-market.php
@@ -1,7 +1,10 @@
-<?php namespace ColdTurkey\DaysOnMarket;
+<?php
+
+namespace ColdTurkey\DaysOnMarket;
+
 /*
  * Plugin Name: Days On Market
- * Version: 1.7.7
+ * Version: 1.7.8
  * Plugin URI: http://www.coldturkeygroup.com/
  * Description: A form for prospective home sellers to fill out to figure out how long it might take them to sell their home.
  * Author: Cold Turkey Group
@@ -14,20 +17,24 @@
  * @since 1.0.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) exit;
+if (!defined('ABSPATH')) {
+    exit;
+}
 
-if ( ! defined( 'DAYS_MARKET_PLUGIN_PATH' ) )
-	define( 'DAYS_MARKET_PLUGIN_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+if (!defined('DAYS_MARKET_PLUGIN_PATH')) {
+    define('DAYS_MARKET_PLUGIN_PATH', trailingslashit(plugin_dir_path(__FILE__)));
+}
 
-if ( ! defined( 'DAYS_MARKET_PLUGIN_VERSION' ) )
-	define( 'DAYS_MARKET_PLUGIN_VERSION', '1.7.7' );
+if (!defined('DAYS_MARKET_PLUGIN_VERSION')) {
+    define('DAYS_MARKET_PLUGIN_VERSION', '1.7.8');
+}
 
-require_once( 'classes/class-days-on-market.php' );
+require_once('classes/class-days-on-market.php');
 
 global $days_market;
-$days_market = new DaysOnMarket( __FILE__, new FrontDesk() );
+$days_market = new DaysOnMarket(__FILE__, new FrontDesk());
 
-if ( is_admin() ) {
-	require_once( 'classes/class-days-on-market-admin.php' );
-	new DaysOnMarket_Admin( __FILE__ );
+if (is_admin()) {
+    require_once('classes/class-days-on-market-admin.php');
+    new DaysOnMarket_Admin(__FILE__);
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -23,6 +23,10 @@ class ComposerStaticInit88b1423541647766479b1a02def86384
             'GuzzleHttp\\Promise\\' => 19,
             'GuzzleHttp\\' => 11,
         ),
+        'C' => 
+        array (
+            'Composer\\Installers\\' => 20,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -42,15 +46,9 @@ class ComposerStaticInit88b1423541647766479b1a02def86384
         array (
             0 => __DIR__ . '/..' . '/guzzlehttp/guzzle/src',
         ),
-    );
-
-    public static $prefixesPsr0 = array (
-        'C' => 
+        'Composer\\Installers\\' => 
         array (
-            'Composer\\Installers\\' => 
-            array (
-                0 => __DIR__ . '/..' . '/composer/installers/src',
-            ),
+            0 => __DIR__ . '/..' . '/composer/installers/src/Composer/Installers',
         ),
     );
 
@@ -65,7 +63,6 @@ class ComposerStaticInit88b1423541647766479b1a02def86384
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit88b1423541647766479b1a02def86384::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit88b1423541647766479b1a02def86384::$prefixDirsPsr4;
-            $loader->prefixesPsr0 = ComposerStaticInit88b1423541647766479b1a02def86384::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit88b1423541647766479b1a02def86384::$classMap;
 
         }, null, ClassLoader::class);
